35cc98097e909e2a5b6c9c374ce4a00b8373a672
Cyber Monitor <Cyber.Monitor@abujaelectricity.com>
2019-03-12 14:31:13 +0100

    models datatype modification

--- trackerreport/models.py
+++ trackerreport/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 class VehicleReport(models.Model):
 	target_name = models.CharField(max_length=50)
-	distance_covered = models.CharField(max_length=50)
-	fuel_consumption = models.CharField(max_length=50)
-	fuel_allocated = models.CharField(max_length=50, blank=True, null=True)
+	distance_covered = models.FloatField(null=True)
+	fuel_consumption = models.FloatField(null=True)
+	fuel_allocated = models.IntegerField(default=0)
 	location = models.CharField(max_length=50, blank=True, null=True)
-	distance_allocated = models.CharField(max_length=100, blank=True, null=True)
-	fuel_per_km = models.CharField(max_length=100, blank=True, null=True)
-	top_speed = models.CharField(max_length=50, blank=True, null=True)
+	distance_allocated = models.IntegerField(default=0)
+	fuel_per_km = models.FloatField(null=True)
+	top_speed = models.IntegerField(default=0)
 	move_duration = models.CharField(max_length=100, blank=True, null=True)
 	stop_duration = models.CharField(max_length=100, blank=True, null=True)
 	move_at = models.CharField(max_length=100, blank=True, null=True)
-	overspeed = models.CharField(max_length=100, blank=True, null=True)
+	overspeed = models.FloatField(null=True)
 	added = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
